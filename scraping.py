e13269d2a918fb7e10b19d98469a0cc1c903886c
Nayan <cnayan789@gmail.com>
2022-05-21 10:39:28 +0545

    modified to work as a module for app.py

--- scraping.py
+++ scraping.py
@@ -1,46 +1,16 @@
 # This file scraps info required for the desktop app
 from bs4 import BeautifulSoup
 import requests
-url = "https://www.hamropatro.com/rashifal"
 
-page = requests.get(url)
-doc = BeautifulSoup(page.text, "html.parser")
-rasifal_container = doc.find_all("div", {"id": "rashifal"})[0]
-your_rasifal = input("Enter your rasifal: ")
+def print_rasifal(index):
 
-#values were assigned by multiple testing from the web page
-rasifal_dict = {
-"mesh"       :0,    
-"mithun"     :2,
-"singha"     :4, 
-"tula"       :6, 
-"dhanu"      :8,
-"khumba"     :10,
-"brish"      :1,
-"karkat"     :3, 
-"kanya"      :5, 
-"brishchik"  :7,     
-"makar"      :9,
-"meen"       :11
-}
+    url = "https://www.hamropatro.com/rashifal"
 
-index = rasifal_dict[your_rasifal] if your_rasifal in rasifal_dict else print("\nInvalid")
+    page = requests.get(url)
+    doc = BeautifulSoup(page.text, "html.parser")
+    rasifal_container = doc.find_all("div", {"id": "rashifal"})[0]
 
-rasifal_class = rasifal_container.find_all("div", {"class": "desc"})[index]
-rasifal = rasifal_class.p
+    rasifal_class = rasifal_container.find_all("div", {"class": "desc"})[index]
+    rasifal = rasifal_class.p
 
-print(rasifal.string)
-
-#incase i need  to write/read using flies
-'''
-string = str(rasifal.string).encode()
-f = open ("test.txt", "wb")
-f.write
-f.write(string)
-f.close()
-
-f = open ("test.txt", "r",encoding="utf-8")
-lines = f.readlines()
-for line in lines:
-    print (line)
-'''
+    print(rasifal.string)
