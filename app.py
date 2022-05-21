47eee8ef6b3c19b1999c4c1df9119279fbb1c86c
Nayan <cnayan789@gmail.com>
2022-05-21 10:40:06 +0545

    can print rasifal to console on cliking the image

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import tkinter as tk
 from tkinter import *
-from PIL import ImageTk, Image  
+from PIL import ImageTk, Image
+from scraping import print_rasifal
 
-def on_click():
-  print("image clicked")
+def on_click(index):
+  print_rasifal(index)
+  print("image clicled")
 
-def insertIamge(image_name,x_cord,y_cord):
+def insertIamge(image_name,x_cord,y_cord,index):
   # Create a photoimage object of the image in the path
   image1 = Image.open(image_name)
-  image1 = image1.resize((167, 220), Image.Resampling.LANCZOS)
+  image1 = image1.resize((167, 165), Image.Resampling.LANCZOS)
   image = ImageTk.PhotoImage(image1)
 
-  b = tk.Button(root, image=image, command=on_click)
+  b = tk.Button(root, image=image, command=lambda: on_click(index))
   b.image = image
 
   # Position image
@@ -19,18 +21,22 @@ def insertIamge(image_name,x_cord,y_cord):
 
 root = Tk()
 root.title("Rasifal")
-root.geometry('501x662')
+root.geometry('501x660')
 
-insertIamge("test.png",0,0)
-insertIamge("test.png",165,0)
-insertIamge("test.png",330,0)
+insertIamge("test.png",0,0,0)
+insertIamge("test.png",167,0,1)
+insertIamge("test.png",334,0,2)
 
-insertIamge("test.png",0,219)
-insertIamge("test.png",165,219)
-insertIamge("test.png",330,219)
+insertIamge("test.png",0,165,3)
+insertIamge("test.png",167,165,4)
+insertIamge("test.png",334,165,5)
 
-insertIamge("test.png",0,438)
-insertIamge("test.png",165,438)
-insertIamge("test.png",330,438)
+insertIamge("test.png",0,330,6)
+insertIamge("test.png",167,330,7)
+insertIamge("test.png",334,330,8)
+
+insertIamge("test.png",0,495,9)
+insertIamge("test.png",167,495,10)
+insertIamge("test.png",334,495,11)
 
 root.mainloop()
